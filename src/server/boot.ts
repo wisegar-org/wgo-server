7b70cc2a843bdb9b2e703773dacb416089d996e9
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2022-04-26 16:47:50 +0200

    Update boot function

--- src/server/boot.ts
+++ src/server/boot.ts
@@ -10,21 +10,14 @@ import { configRouter as configRest } from "../rest/router";
 import { JwtMiddleware } from "../middlewares/JwtMiddleware";
 
 export const boot = async (options: IServerOptions, onStart?: any) => {
-  debugger;
   console.log("Running boot!");
   options.app = options.app ? options.app : express();
-
-  const server = await getApolloServer(options);
-  await server.start();
-  server.applyMiddleware({ app: options.app });
 
   if (options.useCors) {
     options.app.use(cors());
   }
 
   options.app.use(JwtMiddleware(options));
-
-  options.app.use(graphqlUploadExpress());
 
   if (options.middlewares) {
     options.middlewares(options.app);
@@ -34,19 +27,11 @@ export const boot = async (options: IServerOptions, onStart?: any) => {
     configRest(options.controllers, options.app);
   }
 
-  options.app.use(
-    (err: ErrorHandler, req: Request, res: Response, next: NextFunction) => {
-      console.log("Running ErrorHandler use!");
-      const response = JsonResponse(false, err.statusCode || 500, err.message);
-      res.json(response);
-    }
-  );
+  const server = await getApolloServer(options);
+  await server.start();
+  server.applyMiddleware({ app: options.app });
 
-  options.app.use((req: Request, res: Response, next: NextFunction) => {
-    const response = JsonResponse(false, 404, "resource not found");
-    console.log("Running Not found use!");
-    res.json(response);
-  });
+  options.app.use(graphqlUploadExpress());
 
   options.app.listen(options.port, () => {
     if (onStart) onStart();
