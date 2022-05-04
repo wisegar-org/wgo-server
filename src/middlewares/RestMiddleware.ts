6c894880e9132173015c73236d0de5a8f18b4efd
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2022-05-04 18:18:45 +0200

    Remove async UseRestMiddleware

--- src/middlewares/RestMiddleware.ts
+++ src/middlewares/RestMiddleware.ts
@@ -2,7 +2,7 @@ import { IsNullOrUndefined } from "@wisegar-org/wgo-object-extensions";
 import { IServerOptions } from "../interfaces/IServerOptions";
 import { configRouter } from "../rest/router";
 
-export const UseRestMiddleware = async (options: IServerOptions) => {
+export const UseRestMiddleware = (options: IServerOptions) => {
   if (IsNullOrUndefined(options)) throw new Error("Invalid options parameter");
   if (IsNullOrUndefined(options.app))
     throw new Error("Invalid options app parameter");
